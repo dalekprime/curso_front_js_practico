48197424d45e9bc9109c3bdba30652c3e03a0cac
Bryan Silva <bryansvzla@gmail.com>
2022-08-29 03:28:06 -0400

    Siguendo en el camino

--- scripts.js
+++ scripts.js
@@ -1,16 +1,32 @@
 const fraseEmail = document.querySelector(".navbar-email");
 const menuEmail = document.querySelector(".desktop-menu");
+const menuMobileButton = document.querySelector("#mobilebutton");
+const menuMobile = document.querySelector(".mobile-menu");
+const shoppingCart = document.querySelector(".navbar-shopping-cart");
+const menuCart = document.querySelector(".product-detail");
 
-fraseEmail.addEventListener("click", togglemenu);
-document.addEventListener("contextmenu", noMenu);
+fraseEmail.addEventListener("click", function(){
+    toggle(menuEmail);
+    menuCart.classList.add("inactive");
+});
+menuMobileButton.addEventListener("click", function(){
+    toggle(menuMobile);
+    menuCart.classList.add("inactive");
+});
+shoppingCart.addEventListener("click", function(){
+    toggle(menuCart);
+    menuMobile.classList.add("inactive");
+    menuEmail.classList.add("inactive");
+});
+//document.addEventListener("contextmenu", noMenu);
 
-function togglemenu(){
+function toggle(menu){
   /*  if(menuEmail.className != "desktop-menu"){
         menuEmail.classList.remove("inactive");
     }else{
         menuEmail.classList.add("inactive");
     }*/
-    menuEmail.classList.toggle("inactive");
+    menu.classList.toggle("inactive");
 };
 
 function noMenu(event){
